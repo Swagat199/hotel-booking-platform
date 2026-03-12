3fb8b98ea06aa5e5da1f81eed3e788fe76bf3a8f
Swagat <pattanayakswagat2004@gmail.com>
2026-03-12 23:46:02 +0530

    Add models, controllers, and routes for hotel booking functionality

---
+++ server/models/Room.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const roomSchema = mongoose.Schema({
+    hotel:{type:String,ref: "Hotel",required:true},
+    roomType:{type:String,required:true},
+    pricePerNight:{type:Number,required:true},
+    amenities:{type:Array,required:true},
+    images:[{type:String}],
+    isAvailble:{type:Boolean,default:true},
+},{timestamps:true});
+
+const Room = mongoose.model("Room",roomSchema);
+
+export default Room;
