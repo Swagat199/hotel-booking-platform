4a96c1dcaf165d218f9f2abfcfc63267cd1d04ae
Swagat <pattanayakswagat2004@gmail.com>
2026-03-14 12:40:40 +0530

    fix: Refactor multer storage configuration for clarity and maintainability

--- server/middleware/uploadMiddleware.js
+++ server/middleware/uploadMiddleware.js
@@ -1,5 +1,16 @@
 import multer from "multer";
 
-const upload = multer({storage:multer.diskStorage({})})
+
+const storage = multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,"uploads/");
+    },
+    filename:function(req,file,cb){
+        cb(null,Date.now()+"-"+file.originalname);
+    }
+});
+
+/*const upload = multer({storage:multer.diskStorage({})})*/
+const upload = multer({ storage });
 
 export default upload;
